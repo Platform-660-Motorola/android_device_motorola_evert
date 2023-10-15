28f44a7ce28f4212dfc9141a19d75a3b04c8981c
Vhmit <viktor.9630@hotmail.com>
2023-10-15 14:14:22 -0300

    evert: Reorder makefiles
    Change-Id: If2a45e26dbd9e6b090734b890aa7e5c18324723a

--- aosp_evert.mk
+++ aosp_evert.mk
@@ -1,28 +1,19 @@
 #
 # Copyright (C) 2017 The LineageOS Open Source Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common PixelExperience stuff.
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
-# Device
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit from device
 $(call inherit-product, device/motorola/evert/device.mk)
 
 # Bootanimation
@@ -34,9 +25,9 @@ TARGET_GAPPS_ARCH := arm64
 # Device identifiers
 PRODUCT_DEVICE := evert
 PRODUCT_NAME := aosp_evert
-PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g(6) plus
-PRODUCT_MANUFACTURER := motorola
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto G6 Plus
+PRODUCT_MANUFACTURER := Motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=evert
--- device.mk
+++ device.mk
@@ -1,21 +1,11 @@
 #
 # Copyright (C) 2018 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-# Vendor blobs
-$(call inherit-product, vendor/motorola/evert/evert-vendor.mk)
+# Inherit from motorola sdm660-common
+$(call inherit-product, device/motorola/sdm660-common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
@@ -144,5 +134,5 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
-# Inherit from motorola sdm660-common
-$(call inherit-product, device/motorola/sdm660-common/common.mk)
+# Inherit from proprietary files
+$(call inherit-product, vendor/motorola/evert/evert-vendor.mk)
